adae194135a2f8a4dcf1609484a6abd2dcb79dab
Petris Fernandes <petrisrf@gmail.com>
2023-07-11 07:56:50 -0300

    [CHORE] sample route and spec

---
+++ app/controllers/hello_controller.rb
@@ -0,0 +1,5 @@
+class HelloController < ApplicationController
+  def index
+    'hello'
+  end
+end
---
+++ spec/requests/hellos_spec.rb
@@ -0,0 +1,11 @@
+require 'rails_helper'
+
+RSpec.describe "Hellos", type: :request do
+  describe "GET /hello" do
+    it "works!" do
+      get hello_index_path
+      expect(response).to have_http_status(200)
+      expect(response.body).to have_text 'Hello'
+    end
+  end
+end
