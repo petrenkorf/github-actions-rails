533b51e82e34572343bfb1f2a4774ecc62cc2098
Petris Fernandes <petrisrf@gmail.com>
2023-07-11 08:17:45 -0300

    rubocop

--- spec/requests/hellos_spec.rb
+++ spec/requests/hellos_spec.rb
@@ -1,8 +1,10 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 
-RSpec.describe "Hellos", type: :request do
-  describe "GET /hello" do
-    it "works!" do
+RSpec.describe 'Hellos', type: :request do
+  describe 'GET /hello' do
+    it 'works!' do
       get hello_index_path
       expect(response).to have_http_status(200)
     end
